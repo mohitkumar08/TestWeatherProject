6c29fb1fb999f5ed05b924e15bee664d89598390
Mohit Kumar <mca.mohitkumar@gmail.com>
2021-05-26 15:55:45 +0530

    Remove unused class and space

--- core/src/main/java/com/base/BaseComponent.kt
+++ core/src/main/java/com/base/BaseComponent.kt
@@ -1,14 +1,12 @@
 package com.base
 
 import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import com.base.di.module.AppModule
 import com.base.di.module.NetworkModule
 import dagger.Component
 import io.reactivex.disposables.CompositeDisposable
 import retrofit2.Retrofit
 import javax.inject.Singleton
-
 
 @Singleton
 @Component(modules = [AppModule::class, NetworkModule::class])
